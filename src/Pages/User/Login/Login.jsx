fff99cd0ae5f81c0a21cb1af441d5c83b4493433
Minhaj199 <150225109+Minhaj199@users.noreply.github.com>
2024-08-11 08:08:33 +0530

    initail commit and signup added

--- src/Pages/User/Login/Login.jsx
+++ src/Pages/User/Login/Login.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import './Login.css'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+      <div className='login-form'>
+      <h1>User Login</h1>
+      <form >
+      <input type="name" placeholder='User Name' name='useName' />
+      <input type="email" placeholder='Email' name='email' />
+      <input type="tel" placeholder='Mobile' name='phone' />
+      <input type="password" placeholder='Password' name='password'/>
+      <input type="password" placeholder='Confirm password' name='c-password'/>
+      <button type='submit'>Sign in</button>
+      </form>
+      <p>Not Having Account</p>
+      </div>
+            
+    </div>
   )
 }

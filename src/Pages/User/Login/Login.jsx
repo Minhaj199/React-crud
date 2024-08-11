3a660e6c75bc2d5f115e4de42b4d4e625308cafe
Minhaj199 <150225109+Minhaj199@users.noreply.github.com>
2024-08-11 08:56:37 +0530

    Login and signup desing completed

--- src/Pages/User/Login/Login.jsx
+++ src/Pages/User/Login/Login.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
 
 export const Login = () => {
+  const [switching, setSwitching] = useState(false);
+
   return (
-    <div className='login'>
-      <div className='login-form'>
-      <h1>User Login</h1>
-      <form >
-      <input type="name" placeholder='User Name' name='useName' />
-      <input type="email" placeholder='Email' name='email' />
-      <input type="tel" placeholder='Mobile' name='phone' />
-      <input type="password" placeholder='Password' name='password'/>
-      <input type="password" placeholder='Confirm password' name='c-password'/>
-      <button type='submit'>Sign in</button>
-      </form>
-      <p>Not Having Account</p>
+    <div className="login">
+      <div className="login-form">
+        <h1>{switching?'User Signup':'User Login'}</h1>
+        <form>
+          {switching ? (
+            <input type="name" placeholder="User Name" name="useName" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email" name="email" />
+          {switching ? (
+            <input type="tel" placeholder="Mobile" name="phone" />
+          ) : (
+            <></>
+          )}{" "}
+          <input type="password" placeholder="Password" name="password" />
+          {switching ? (
+            <input
+              type="password"
+              placeholder="Confirm password"
+              name="c-password"
+            />
+          ) : (
+            <></>
+          )}
+          <button type="submit">{switching ? "Sign up" : "Sign in"}</button>
+        </form>
+
+        <p className={switching?'signup-stage ':'login-stae'} onClick={() => setSwitching((previous) => !previous)}>
+        {switching?'Already have an account ':'Not Having Account '}
+        </p>
       </div>
-            
     </div>
-  )
-}
+  );
+};
